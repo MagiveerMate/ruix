9e00544b2ec570a3fe281f6cfa55c3370c515b6c
Jarred <jarredvonallemann@gmail.com>
2024-02-05 17:54:28 +0200

    updated

--- register/src/SignUpPage.js
+++ register/src/SignUpPage.js
@@ -52,7 +52,7 @@ const SignUpPage = ({ onSuccessfulRegistration }) => {
     <div className="signup-container">
       <form className="signup-form" onSubmit={handleSubmit}>
         <div className="logo-section">
-        <img/>
+        <img alt='logo'/>
         </div>
         <div>
           <h2 className='sp'>SIGN UP</h2>
@@ -88,7 +88,7 @@ const SignUpPage = ({ onSuccessfulRegistration }) => {
         </p>
       </form>
       <div className='side-img'>
-      <img/>
+      <img alt='img'/>
       </div>
     </div>
   );
